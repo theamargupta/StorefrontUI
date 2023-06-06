e6752affe4bc7aa0a683fd19c064252f7a97edb0
Amar Gupta <theamargupta.tech@gmail.com>
2023-06-07 03:36:25 +0530

    changes pushed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,18 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 const { tailwindConfig } = require('@storefront-ui/react/tailwind-config');
 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   presets: [tailwindConfig],
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx}',
+    './src/app/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{js,ts,jsx,tsx}',
     './node_modules/@storefront-ui/react/**/*.{js,mjs}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+    extend: {},
   },
   plugins: [],
-}
+};
